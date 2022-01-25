2b1a2ef8d442108b114c02507af8b4b03c6711b7
pavan <pavangurram56@gmail.com>
2022-01-26 00:11:04 +0530

    modified

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ function AboutPage() {
   return (
     <Card>
       <h1>About</h1>
-      <p>This App Is Built Using React with ❤️</p>
+      <p>This App Is Built Using React with ❤️ by Pavan Kumar</p>
       <p>Version 1.0.0</p>
       <Link to="/">Back to Home</Link>{" "}
       {/*if we use <a></a> then it refreshes the browser every time we click*/}
