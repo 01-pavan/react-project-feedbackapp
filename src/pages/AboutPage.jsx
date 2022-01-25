86157ae4b69c9829b39de0d3219452357936b884
pavan <pavangurram56@gmail.com>
2022-01-25 21:02:14 +0530

    about page added

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import Card from "../components/shared/Card";
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About</h1>
+      <p>This App Is Built Using React with ❤️</p>
+      <p>Version 1.0.0</p>
+    </Card>
+  );
+}
+
+export default AboutPage;
